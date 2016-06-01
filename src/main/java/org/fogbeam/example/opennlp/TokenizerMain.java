18ddec3b4a6e61de1a6171a536179e1c7fc6f3c0
Samuel Dóniz <alu0100697315@ull.edu.es>
2016-06-01 17:52:52 +0100

    Lee UN fichero, separa y crea fichero

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -6,9 +6,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.Scanner;
 
@@ -29,9 +31,12 @@ public class TokenizerMain {
 			Tokenizer tokenizer = new TokenizerME(model);
 			Scanner s = new Scanner(System.in);
 			String aux = null;
+			FileWriter fichero = null;
+			PrintWriter pw = null;
 			Writer out=null;
 			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("fichero_salida.txt"),"UTF-8"));
-					
+			fichero= new FileWriter ("fichero.txt");
+			pw = new PrintWriter(fichero);
 		while(aux != "s"){
 		System.out.println("Introduce el nombre de un fichero: ");
 		System.out.println("Si quiere salir pulse 's'");
@@ -45,8 +50,14 @@ public class TokenizerMain {
 			String[] tokens = null;
 			while((cadena = b.readLine())!=null) {
 				tokens = tokenizer.tokenize(cadena);
-				for( String token : tokens ){
-				out.write(token);
+				
+				for(int i=0;i<tokens.length;i++){
+			//	for( String token : tokens ){
+				System.out.println(tokens[i]);
+			//	out.write(token);
+				for (int j = 0; j < tokens.length; j++)
+	                pw.println(tokens[j]);
+				
 				}
 			}		
 			b.close();
