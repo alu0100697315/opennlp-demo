35829d69e235bda16374f4c9138f14725ea42ac0
Samuel Dóniz <alu0100697315@ull.edu.es>
2016-06-10 10:18:46 +0100

    Creados los comentarios del codigo

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -12,52 +12,75 @@ import java.util.Scanner;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
-
+/**
+ * Clase TokenizerMain. Perteneciente al proyecto OpenNLP
+ * Clase que integra los metodos para procesar uno o varios ficheros, separar en tokens y almacenarlo en un fichero.
+ * @author Samuel
+ *
+ */
 public class TokenizerMain {
-	
+	/**
+	 * Metodo obtiene los distintos token que forman cierto fichero de texto
+	 * Luego escribe los tokens en un fichero de salida.
+	 * @param pw
+	 * @param tokens
+	 * @param b
+	 * @param tokenizer
+	 * @throws IOException
+	 */
 	public static void Tokenizer(PrintWriter pw,String[] tokens,BufferedReader b,Tokenizer tokenizer) throws IOException{
 		String cadena;
-		while((cadena = b.readLine())!=null) {
-			tokens = tokenizer.tokenize(cadena);
+		while((cadena = b.readLine())!=null) { //mientras no se llegue al final del fichero
+			tokens = tokenizer.tokenize(cadena); //almacena en "tokens" los tokens
 			
 			for( String token : tokens ){	
-				pw.println(token);
+				pw.println(token); //escribe en el fichero los tokens
 				pw.flush();
 			}	
 		}
 	}
-	
+	/**
+	 * Metodo para mostrar el menu que indica al usuario que debe hacer
+	 * @param aux
+	 * @param s
+	 * @return
+	 */
 	public static String LeerFichero (String aux,Scanner s){
 		System.out.println(" ");
 		System.out.println("Introduce el nombre de un fichero: ");
 		System.out.println("Si quiere salir pulse 's'");
 		return aux;
 	}
-	
+	/**
+	 * Funcion principal. 
+	 * Se inicializa las distintas variables que vamos a necesitar y se llama a las funciones necesarias
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main( String[] args ) throws Exception{
 		InputStream modelIn = new FileInputStream( "models/en-token.model" );
 		try{
 			TokenizerModel model = new TokenizerModel( modelIn );
 			Tokenizer tokenizer = new TokenizerME(model);
 			
-			Scanner s = new Scanner(System.in);
-			String aux = null;
-			FileWriter fichero = null;
-			PrintWriter pw = null;
+			Scanner s = new Scanner(System.in); //variable para leer desde teclado
+			String aux = null; //variable para almacenar el nombre del fichero
+			FileWriter fichero = null; //variable para crear el fichero de salida
+			PrintWriter pw = null; //variable para escribir en fichero de salida
 			fichero= new FileWriter ("fichero.txt");
 			pw = new PrintWriter(fichero);
 			
-		while(aux != "s"){
+		while(aux != "salir"){ //Con este bucle while permitimos que se puedas incluir varios ficheros
 		LeerFichero(aux,s);	
 		aux=s.nextLine();
-
+		
 		File archivo = new File(aux);
 		FileReader f = new FileReader(archivo);
 		BufferedReader b = new BufferedReader(f);
 		
 			String[] tokens = null;
-			Tokenizer(pw,tokens,b, tokenizer);
-			pw.println("\n---OTRO FICHERO --- \n");
+			Tokenizer(pw,tokens,b, tokenizer); //llamada al metodo que separa los tokens y genera el fichero
+			pw.println("\n---OTRO FICHERO --- \n"); //Separador en caso de que ingresemos otro fichero
 			b.close();
 		}
 				pw.close();
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -2,32 +2,48 @@ import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 import org.fogbeam.example.opennlp.TokenizerMain;
 import org.junit.Test;
 
-
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 
 
 public class MyTests {
 	
+	
 	@org.junit.Test
-	public final void testMain() {
+	/**
+	 * Test para un objeto de tipo TokenizerMain
+	 */
+	public final void testMain(){
 		TokenizerMain t = new TokenizerMain();
-		assertTrue(t != null); // TODO
+		assertTrue(t != null);
 	}
 
 	@Test
+	/**
+	 * Test comprobar si crea bien un fichero
+	 */
 	public void CrearFichero() {
 		File archivo = new File("hola.txt");
 		assertTrue(archivo != null);
 	}
 	
 	@Test
+	/**
+	 * Test para comprobar si se lee bien un fichero de entrada
+	 * @throws FileNotFoundException
+	 */
 	public void LecturaFichero() throws FileNotFoundException {
 		System.out.println("Probando si se lee el fichero");
 		File archivo = new File("hola.txt");
@@ -36,6 +52,12 @@ public class MyTests {
 		assertTrue( f != null);
 	}
 	
-	 
-
+	/**
+	 * Test para el metodo menu
+	 */
+	@Test
+	public void menu(){
+		Scanner s = new Scanner(System.in);
+		assertTrue(TokenizerMain.LeerFichero("hola.txt", s) != null);
+	}
 }
